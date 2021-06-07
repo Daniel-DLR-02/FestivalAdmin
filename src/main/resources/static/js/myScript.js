c76d51f82f8092fb122e77ce5027b468711ab4fb
Daniel <danidlr02@gmail.com>
2021-06-07 11:36:37 +0200

    Añado la validación de número de telefono.

--- src/main/resources/static/js/myScript.js
+++ src/main/resources/static/js/myScript.js
@@ -14,10 +14,22 @@ function validarNombreApellido(){
 
 function validarNumeroTelefono(){
    
+    let campoNumeroTelefono = document.getElementById('error_numeroTelefono')
     let numeroTelefono=document.getElementById('numTelefono').value;
+    let mensajeError=document.getElementById('error_numeroTelefono');
 
-    alert(numeroTelefono);
+    let inputNumeroDeTelefono=document.getElementById('numTelefono');
 
-    if(numeroTelefono.length!=9)
-        alert("Error.")
+    if((numeroTelefono.length!=9 || numeroTelefono[0]==0 || numeroTelefono[0]==1 || numeroTelefono[0]==2 
+        || numeroTelefono[0]==3 || numeroTelefono[0]==4 || numeroTelefono[0]==5 || numeroTelefono[0]==0)){
+        mensajeError.classList = 'formulario__input-error-activo';
+        inputNumeroDeTelefono.classList = 'formulario_marco_error form-control';
+    }
+    else if((numeroTelefono.length==9 || numeroTelefono[0]!=0 || numeroTelefono[0]!=1 || numeroTelefono[0]!=2
+        || numeroTelefono[0]!=3 || numeroTelefono[0]!=4 || numeroTelefono[0]!=5 || numeroTelefono[0]!=0)){
+        mensajeError.classList = 'formulario__input-error';
+        inputNumeroDeTelefono.classList = 'formulario__input form-control';
+    }
+
+        
 }
