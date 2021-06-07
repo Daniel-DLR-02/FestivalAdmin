c74fc8ddd935ea3cfb771142dd2a7639e25a0c82
Daniel De Luna Rodríguez <deluna.rodan21@aulas.triana>
2021-06-07 09:00:25 +0200

    Empiezo con la validación de formulario.

--- src/main/resources/static/js/myScript.js
+++ src/main/resources/static/js/myScript.js
@@ -3,10 +3,21 @@ function contarElementosTabla(){
    
     let contadorFilas = document.getElementById("numeroFilas");
 
-    let numeroFilas = $('table tr').length-2;
+    let numeroFilas = document.querySelectorAll('table tr').length-2;
     
     contadorFilas.innerHTML=numeroFilas;
-    return numeroFilas;
 }
 
-contarElementosTabla();
+function validarNombreApellido(){
+
+}
+
+function validarNumeroTelefono(){
+   
+    let numeroTelefono=document.getElementById('numTelefono').value;
+
+    alert(numeroTelefono);
+
+    if(numeroTelefono.length!=9)
+        alert("Error.")
+}
