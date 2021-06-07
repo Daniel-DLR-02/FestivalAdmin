18f117df653b60cbdbfd954ea78f72e54e84b869
Daniel <danidlr02@gmail.com>
2021-06-07 18:23:15 +0200

    Arreglo validación de teléfono y formulario de eventos

--- src/main/resources/static/js/myScript.js
+++ src/main/resources/static/js/myScript.js
@@ -16,20 +16,26 @@ function validarNumeroTelefono(){
    
     let campoNumeroTelefono = document.getElementById('error_numeroTelefono')
     let numeroTelefono=document.getElementById('numTelefono').value;
+    for(let i=0;i<numeroTelefono.length;i++)
+        numeroTelefono = numeroTelefono.replace(" ","");
     let mensajeError=document.getElementById('error_numeroTelefono');
-
     let inputNumeroDeTelefono=document.getElementById('numTelefono');
+    let grupoFormulario = document.getElementById("marco_input-telefono");
 
-    if((numeroTelefono.length!=9 || numeroTelefono[0]==0 || numeroTelefono[0]==1 || numeroTelefono[0]==2 
-        || numeroTelefono[0]==3 || numeroTelefono[0]==4 || numeroTelefono[0]==5 || numeroTelefono[0]==0)){
+    if(numeroTelefono.length!=9 || numeroTelefono[0]==0 || numeroTelefono[0]==1 || numeroTelefono[0]==2 
+        || numeroTelefono[0]==3 || numeroTelefono[0]==4 || numeroTelefono[0]==5){
         mensajeError.classList = 'formulario__input-error-activo';
-        inputNumeroDeTelefono.classList = 'formulario_marco_error form-control';
+        grupoFormulario.classList='formulario__grupo-incorrecto';
     }
-    else if((numeroTelefono.length==9 || numeroTelefono[0]!=0 || numeroTelefono[0]!=1 || numeroTelefono[0]!=2
-        || numeroTelefono[0]!=3 || numeroTelefono[0]!=4 || numeroTelefono[0]!=5 || numeroTelefono[0]!=0)){
+    else if(numeroTelefono.length==9 || numeroTelefono[0]!=0 || numeroTelefono[0]!=1 || numeroTelefono[0]!=2
+        || numeroTelefono[0]!=3 || numeroTelefono[0]!=4 || numeroTelefono[0]!=5 ){
         mensajeError.classList = 'formulario__input-error';
-        inputNumeroDeTelefono.classList = 'formulario__input form-control';
+        grupoFormulario.classList='formulario__grupo-input'
     }
 
         
+}
+
+function validarCorreoElectronico(){
+    
 }
