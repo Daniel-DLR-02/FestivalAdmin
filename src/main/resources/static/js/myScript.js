77c1b94b3694b02f4900e977665804f4c43b0ca6
Daniel De Luna Rodríguez <deluna.rodan21@aulas.triana>
2021-06-10 14:29:20 +0200

    Sigo añadiendo funciones para la validacion de formularios,ahora sobre todo en eventos.

--- src/main/resources/static/js/myScript.js
+++ src/main/resources/static/js/myScript.js
@@ -1,113 +1,173 @@
 
-function contarElementosTabla(){
-   
+function contarElementosTabla() {
+
     let contadorFilas = document.getElementById("numeroFilas");
 
-    let numeroFilas = document.querySelectorAll('table tr').length-2;
-    
-    contadorFilas.innerHTML=numeroFilas;
+    let numeroFilas = document.querySelectorAll('table tr').length - 2;
+
+    contadorFilas.innerHTML = numeroFilas;
 }
 
 contarElementosTabla();
 
-function validarNombre(){
-    let patronNombre =/^[a-zA-Z ]{2,30}$/;
+function validarNombre() {
+    let patronNombre = /^[a-zA-Z ]{2,30}$/;
 
-    let nombre = document.getElementById('nombre').value;  
-    let mensajeError=document.getElementById('error_nombre');
+    let nombre = document.getElementById('nombre').value;
+    let mensajeError = document.getElementById('error_nombre');
     let grupoFormulario = document.getElementById("marco_input-nombre");
-    if(!patronNombre.test(String(nombre).toLowerCase())){
+    if (!patronNombre.test(String(nombre).toLowerCase())) {
         mensajeError.classList = 'formulario__input-error-activo';
-        grupoFormulario.classList='formulario__grupo-incorrecto';
+        grupoFormulario.classList = 'formulario__grupo-incorrecto';
+        return false;
     }
-    else if(patronNombre.test(String(nombre).toLowerCase())){
+    else if (patronNombre.test(String(nombre).toLowerCase())) {
         mensajeError.classList = 'formulario__input-error';
-        grupoFormulario.classList='formulario__grupo-input';
+        grupoFormulario.classList = 'formulario__grupo-input';
+        return true;
     }
 }
 
-function validarApellidos(){
-    let patronApellidos =/^[a-zA-Z ]{2,30}$/;
+function validarApellidos() {
+    let patronApellidos = /^[a-zA-Z ]{2,30}$/;
 
-    let apellidos = document.getElementById('apellidos').value;  
-    let mensajeError=document.getElementById('error_apellidos');
+    let apellidos = document.getElementById('apellidos').value;
+    let mensajeError = document.getElementById('error_apellidos');
     let grupoFormulario = document.getElementById("marco_input-apellidos");
 
-    if(!patronApellidos.test(String(apellidos).toLowerCase())){
+    if (!patronApellidos.test(String(apellidos).toLowerCase())) {
         mensajeError.classList = 'formulario__input-error-activo';
-        grupoFormulario.classList='formulario__grupo-incorrecto';
+        grupoFormulario.classList = 'formulario__grupo-incorrecto';
+        return false;
     }
-    else if(patronApellidos.test(String(apellidos).toLowerCase())){
+    else if (patronApellidos.test(String(apellidos).toLowerCase())) {
         mensajeError.classList = 'formulario__input-error';
-        grupoFormulario.classList='formulario__grupo-input';
+        grupoFormulario.classList = 'formulario__grupo-input';
+        return true;
     }
 }
 
-function validarNumeroTelefono(){
-   
-    let numeroTelefono=document.getElementById('numTelefono').value;
-    for(let i=0;i<numeroTelefono.length;i++)
-        numeroTelefono = numeroTelefono.replace(" ","");
-    numeroTelefono=parseInt(numeroTelefono);
-    let mensajeError=document.getElementById('error_numeroTelefono');
+function validarNumeroTelefono() {
+
+    let numeroTelefono = document.getElementById('numTelefono').value;
+    for (let i = 0; i < numeroTelefono.length; i++)
+        numeroTelefono = numeroTelefono.replace(" ", "");
+    numeroTelefono = parseInt(numeroTelefono);
+    let mensajeError = document.getElementById('error_numeroTelefono');
     let grupoFormulario = document.getElementById("marco_input-telefono");
 
-    if(numeroTelefono<=600000000 || numeroTelefono >= 999999999 || Number.isNaN(numeroTelefono)){
+    if (numeroTelefono <= 600000000 || numeroTelefono >= 999999999 || Number.isNaN(numeroTelefono)) {
         mensajeError.classList = 'formulario__input-error-activo';
-        grupoFormulario.classList='formulario__grupo-incorrecto';
+        grupoFormulario.classList = 'formulario__grupo-incorrecto';
+        return false;
     }
-    else if(numeroTelefono>=600000000 && numeroTelefono <= 999999999){
+    else if (numeroTelefono >= 600000000 && numeroTelefono <= 999999999) {
         mensajeError.classList = 'formulario__input-error';
-        grupoFormulario.classList='formulario__grupo-input';
+        grupoFormulario.classList = 'formulario__grupo-input';
+        return true;
     }
 
-        
+
 }
 
-function validarCorreoElectronico(){
+function validarCorreoElectronico() {
 
-    let email = document.getElementById('correoElectronico').value;  
+    let email = document.getElementById('correoElectronico').value;
 
-    let mensajeError=document.getElementById('error_correoElectronico');
+    let mensajeError = document.getElementById('error_correoElectronico');
     let grupoFormulario = document.getElementById("marco_input-correoElectronico");
 
     let patronCorreo = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-    if (!patronCorreo.test(String(email).toLowerCase())){
+    if (!patronCorreo.test(String(email).toLowerCase())) {
         mensajeError.classList = 'formulario__input-error-activo';
-        grupoFormulario.classList='formulario__grupo-incorrecto';
+        grupoFormulario.classList = 'formulario__grupo-incorrecto';
+        return false;
     }
-    else if (patronCorreo.test(String(email).toLowerCase())){
+    else if (patronCorreo.test(String(email).toLowerCase())) {
         mensajeError.classList = 'formulario__input-error';
-        grupoFormulario.classList='formulario__grupo-input';
+        grupoFormulario.classList = 'formulario__grupo-input';
+        return true;
     }
 }
 
-function validarPrecioEntrada(){
-    let patronAEvitar =/^[0-9]+([.][0-9]+)?$/;  
+function validarPrecioEntrada() {
+    let patronAEvitar = /^[0-9]+([.][0-9]+)?$/;
 
-    let precio = document.getElementById('precioEntrada').value;  
+    let precio = document.getElementById('precioEntrada').value;
 
-    let mensajeError=document.getElementById('error_precioEntrada');
+    let mensajeError = document.getElementById('error_precioEntrada');
     let grupoFormulario = document.getElementById("marco_input-precioEntrada");
 
-    if(!patronAEvitar.test(String(precio).toLowerCase())){
+    if (!patronAEvitar.test(String(precio).toLowerCase())) {
         mensajeError.classList = 'formulario__input-error-activo';
-        grupoFormulario.classList='formulario__grupo-incorrecto';
+        grupoFormulario.classList = 'formulario__grupo-incorrecto';
+        return false;
+    }
+    else if (patronAEvitar.test(String(precio).toLowerCase())) {
+        mensajeError.classList = 'formulario__input-error';
+        grupoFormulario.classList = 'formulario__grupo-input';
+        return true;
     }
 
 
 }
 
-function validarDescripcion(){
-    
+function validarDescripcion() {
+
     let descripcion = document.getElementById('descripcion').value;
 
-    let mensajeError=document.getElementById('error_descripcion');
+    let mensajeError = document.getElementById('error_descripcion');
     let grupoFormulario = document.getElementById("marco_input-descripcion");
-
-    alert(descripcion);
-    if(descripcion.length>255){
+    if (descripcion.length > 255) {
         mensajeError.classList = 'formulario__input-error-activo';
-        grupoFormulario.classList='formulario__grupo-incorrecto';
+        grupoFormulario.classList = 'formulario__grupo-incorrecto';
+        return false;
+    }
+    else if (descripcion.length <= 255) {
+        mensajeError.classList = 'formulario__input-error';
+        grupoFormulario.classList = 'formulario__grupo-input';
+        return true;
     }
 }
+
+function validarURLFoto() {
+    let url = document.getElementById('fotoEvento').value;
+
+    let mensajeError = document.getElementById('error_fotoEvento');
+    let grupoFormulario = document.getElementById("marco_input-fotoEvento");
+
+    if (url.length > 255) {
+        mensajeError.classList = 'formulario__input-error-activo';
+        grupoFormulario.classList = 'formulario__grupo-incorrecto';
+        return false;
+    }
+    else if (url.length <= 255) {
+        mensajeError.classList = 'formulario__input-error';
+        grupoFormulario.classList = 'formulario__grupo-input';
+        return true;
+    }
+}
+
+function validarTextoAlternativo() {
+    let textAlt = document.getElementById('textoAlternativo').value;
+
+    let mensajeError = document.getElementById('error_textoAlternativo');
+    let grupoFormulario = document.getElementById("marco_input-textoAlternativo");
+
+    if (textAlt.length > 255) {
+        mensajeError.classList = 'formulario__input-error-activo';
+        grupoFormulario.classList = 'formulario__grupo-incorrecto';
+        return false;
+    }
+    else if (textAlt.length <= 255) {
+        mensajeError.classList = 'formulario__input-error';
+        grupoFormulario.classList = 'formulario__grupo-input';
+        return true;
+    }
+}
+
+function validarFormularioDeReserva(){
+    
+}
+
+
